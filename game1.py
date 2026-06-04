af046cb2acc3d66c19c399454e4f451a885443f6
Aakidul <157734003+Aakidul@users.noreply.github.com>
2026-06-05 00:47:04 +0530

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,34 @@
+import random
+import os
+
+
+os.system("clear")
+
+logo = """
+WECOME TO POOL OF NUMBERS GAME, YOU ENTER RANDOM NUMBERS BETWEEN 1 TO 1000 AND IF THAT EXISTS IN THE GAMES POOL COLLECTION OF 100 YOU WIN
+
+"""
+
+
+
+def pool():
+    pool_of_numbers = []
+    for i in range(1, 101):
+        r = random.randint(1, 1000)
+        pool_of_numbers.insert(i, int(r))
+        if i == 100:
+            break
+
+
+    return pool_of_numbers
+
+
+while True:
+    i = int(input("ENTER LUCKY NUMBER:<  "))
+    pool_data = pool()
+
+    if i in pool_data:
+        print('YOU WON')
+
+    else:
+        print('LOST')
