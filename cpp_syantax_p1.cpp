41bf5f52749233627d6f60506ee11bf56e317662
Aakidul <157734003+Aakidul@users.noreply.github.com>
2026-06-26 23:03:31 +0530

    Create cpp_syantax_p1.cpp

---
+++ cpp_syantax_p1.cpp
@@ -0,0 +1,26 @@
+//BASICS 1, DATA TYPES
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+//DEFINING DATATYPE
+
+    string name = "kalimask"; //string
+    char charA = 'A';        //char
+    int score = 67;          //integer
+    double price = 67.67;      //double aka float
+    bool GameOver = true;    //boolen
+
+
+//PRINTING
+    cout << name << endl; //string printing
+    cout << charA << endl; //char printing
+    cout << score << endl; //integer printing
+    cout << price << endl; //double printing aka float
+    cout << GameOver << endl; //Boolen printing
+
+    return 0;
+}
