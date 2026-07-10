4c4c3f41fe5c6e6afae13d0603a9c548ea44d649
Aakidul <157734003+Aakidul@users.noreply.github.com>
2026-07-10 23:56:47 +0530

    Update 2_sum_problem_python3.py
    Added detailed discription

--- 2_sum_problem_python3.py
+++ 2_sum_problem_python3.py
@@ -9,5 +9,18 @@ def sum_dsa(target, array):
             if array[i] + array[x] == target:
                return [i, x]
 
+"""
 a = sum_dsa(12, [1, 3, 9, 10, 11, 5])
 print(a)
+"""
+***things to remember are***
+
+   selection we use combination and order doesn't matter 
+   so, it will always match the target if there are 2 indices which
+   equals to the target. and for onlt one valid answer as per rule we dont 
+   need orders to check, just need one valid answer and it already exists in
+   linear list.
+
+   and if problem is about order then we use permutations.
+"""
+"""
